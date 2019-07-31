dd0eb4ffabff90175e2b7595734aa2dcc11cf308
David Bailey <davidsbailey@users.noreply.github.com>
2019-07-31 13:16:23 -0700

    move firehose init into FirehoseClient constructor

--- apps/src/lib/util/firehose.js
+++ apps/src/lib/util/firehose.js
@@ -56,6 +56,10 @@ const deliveryStreamName = 'analysis-events';
 // TODO(asher): Determine whether any of the utility functions herein should be
 // moved elsewhere, e.g., to apps/src/util.js.
 class FirehoseClient {
+  constructor() {
+    this.firehose = createNewFirehose();
+  }
+
   /**
    * Returns the current environment.
    * @return {string} The current environment, e.g., "staging" or "production".
@@ -223,7 +227,7 @@ class FirehoseClient {
       return;
     }
 
-    FIREHOSE.putRecord(
+    this.firehose.putRecord(
       {
         DeliveryStreamName: deliveryStreamName,
         Record: {
@@ -270,7 +274,7 @@ class FirehoseClient {
       };
     });
 
-    FIREHOSE.putRecordBatch(
+    this.firehose.putRecordBatch(
       {
         DeliveryStreamName: deliveryStreamName,
         Records: batch
@@ -316,6 +320,5 @@ function createNewFirehose() {
   });
 }
 
-const FIREHOSE = createNewFirehose();
 const firehoseClient = new FirehoseClient();
 export default firehoseClient;
